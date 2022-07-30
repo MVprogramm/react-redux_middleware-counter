2072f67aa3ecda8e53bcaf9f313da984ae40bdab
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-07-30 17:28:29 +0300

    add code

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from "redux";
+import { counterReducer } from "./counter.reducer.js";
+
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+
+  return result;
+}
+
+const store = createStore(
+  counterReducer,
+  applyMiddleware(logger)
+);
+
+export default store;
